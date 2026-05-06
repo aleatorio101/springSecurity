f12ffaf4402165c5314bb49f6c5bf878af7a4c0b
aleatorio101 <lukasdng@gmail.com>
2026-05-05 22:03:26 -0300

    ajustes gerais

--- securitySpring/src/main/java/com/dsl/securitySpring/repositorys/UserRepository.java
+++ securitySpring/src/main/java/com/dsl/securitySpring/repositorys/UserRepository.java
@@ -2,9 +2,10 @@ package com.dsl.securitySpring.repositorys;
 
 import com.dsl.securitySpring.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+    UserDetails findByLogin(String role);
 
 }
