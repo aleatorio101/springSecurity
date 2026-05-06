5aab724cc67882856d918019a0462bb11d872c61
lucasD <lukasdng@gmail.com>
2026-05-05 22:56:54 -0300

    anotacao bean

--- securitySpring/src/main/java/com/dsl/securitySpring/configuration/SecurityConfiguration.java
+++ securitySpring/src/main/java/com/dsl/securitySpring/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.dsl.securitySpring.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
