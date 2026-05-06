0022149cbf525a816267b7ba8860c159b73d79ec
aleatorio101 <lukasdng@gmail.com>
2026-05-05 22:20:27 -0300

    ajustes de segurança

--- securitySpring/src/main/java/com/dsl/securitySpring/clsSecuritySpringApplication.java
+++ securitySpring/src/main/java/com/dsl/securitySpring/clsSecuritySpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class clsSecuritySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuritySpringApplication.class, args);
+		SpringApplication.run(clsSecuritySpringApplication.class, args);
 	}
 
 }
